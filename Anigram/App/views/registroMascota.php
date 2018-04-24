912af7c845604cb31cd6cb40c16d225f8b753b30
Miriam <mirlop01@ucm.es>
2018-04-24 16:57:13 +0200

    Selector tipo mascota presioso

--- Anigram/App/views/registroMascota.php
+++ Anigram/App/views/registroMascota.php
@@ -18,43 +18,29 @@
 			<h2>Foto perfil mascota</h2>
 		</div>
 
-		<div class="tipomascota">
+		<!-- <div class="tipomascota">
 			Perro <input type="radio" name="tipo" value="1" checked>
 			Gato <input type="radio" name="tipo" value="2">			
 			Conejo <input type="radio" name="tipo" value="3">
 			Hamster <input type="radio" name="tipo" value="4">
 			Ave <input type="radio" name="tipo" value="5">
 			Cobaya <input type="radio" name="tipo" value="6">		 
-        </div>
+        </div> -->
 
-		<!-- PRUEBA SELECTOR TIPO MASCOTA
-	 style="background-image:url('/Anigram/Desarrollo/img/IMG_0547.jpg')"
-		<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-			<div class="carousel-inner">
-				<div class="carousel-item active">
-				<img class="d-block w-30" src="../../public/img/perro-pixilart.png" alt="First slide">
-				</div>
-				<div class="carousel-item">
-				<img class="d-block w-30" src="../../public/img/gato-pixilart.png" alt="Second slide">
-				</div>
-				<div class="carousel-item">
-				<img class="d-block w-30" src="../../public/img/conejo-pixilart.png" alt="Third slide">
-				</div>
+		<div class="dropdown">
+			<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				Dropdown button
+			</button>
+			<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				<a class="dropdown-item" href="#">Action</a>
+				<a class="dropdown-item" href="#">Another action</a>
+				<a class="dropdown-item" href="#">Something else here</a>
 			</div>
-				<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span class="sr-only">Previous</span>
-			</a>
-				<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-					<span class="carousel-control-next-icon" aria-hidden="true"></span>
-					<span class="sr-only">Next</span>
-			</a>
 		</div>
-		--> 
         
-		<div class="biomascota">
+		<!-- <div class="biomascota">
 			<textarea name="bio" class="formulario-textbox-mascota" rows="5" placeholder="Descripción" cols="30" ></textarea>
-		</div>
+		</div> -->
         <div id="boton_enviar">
             <input id="submitMascota" type="submit" name="submit" class="submitHueso" value="Añadir"/>	
         </div>
