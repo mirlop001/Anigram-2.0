ffef969c1deb1a5f84dcc81b3d43b0667dcd3886
Enrique <enrifuer@ucm.es>
2018-05-08 21:44:48 +0200

    index terminado

--- Anigram/index.php
+++ Anigram/index.php
@@ -1,5 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Anigram</title>
+</head>
+<body>
+    <?php include 'App/views/Comun/cabecera.php'; ?>
+    <h1>Bienvenido a Anigram</h1>
 
-echo phpinfo();
+    <div id="boton_enviar">
+        <button class="submitHueso" onclick="location='App/views/login.php'">Acceder</button>
+    </div>
 
-?>
+
+</body>
+</html>
