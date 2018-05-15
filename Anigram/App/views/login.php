1cca3c560a308d5aa980126bd837ff7047f267f5
Miriam <mirlop01@ucm.es>
2018-05-15 23:27:30 +0200

    Corrección vistas responsive en pantallas de login y registro

--- Anigram/App/views/login.php
+++ Anigram/App/views/login.php
@@ -14,28 +14,26 @@
 </head>
 
 <body>
-<img id="logo-grande" src="../../public/img/Logo-Nombre.png" alt="">
-
-	<div class="container ">
-			<div class="col align-self-center col-lg-8 offset-lg-2">
-			<h1>Login</h1>
-
-				<form id="form-login" class="centered" method="POST" action='../controllers/login_controller.php'>
-					<label  >Email</label>
-    				<label class='error-form usuarioNoExiste'>Usuario o contraseña erróneos</label>
-					<input id="email-login" type="email" name="user" class="formulario-textbox"/>
-					<label for="Clave">Contraseña</label>
-					<input id="Clave-login" type="password" name="password" class="formulario-textbox"/>
-					<div id="boton_enviar">
-						<input id="submit" type="submit" name="submit" class="submitHueso" value="Entrar"/>
-					</div>
-				</form>
-				<div class="col offset-lg-4 offset-md-3 offset-2 references">
-					<span ><a href="registro.php">Registrarme</a></span>
-					<span id="vertical-separator">|</span>
-					<span ><a href="home.php">Entrar como invitado</a></span>
-				</div>
+	<img id="logo-grande" src="../../public/img/Logo-Nombre.png" alt="">
+	<div class="container-login">
+		<!-- <div class="col align-self-center col-lg-8 offset-lg-2"> -->
+		<h1>Login</h1>
+		<form id="form-login" class="centered" method="POST" action='../controllers/login_controller.php'>
+			<label  >Email</label>
+			<label class='error-form usuarioNoExiste'>Usuario o contraseña erróneos</label>
+			<input id="email-login" type="email" name="user" class="formulario-textbox"/>
+			<label for="Clave">Contraseña</label>
+			<input id="Clave-login" type="password" name="password" class="formulario-textbox"/>
+			<div id="boton_enviar">
+				<input id="submit" type="submit" name="submit" class="submitHueso" value="Entrar"/>
 			</div>
+		</form>
+		<div class="references">
+			<span ><a href="registro.php">Registrarme</a></span>
+			<span id="vertical-separator">|</span>
+			<span ><a href="home.php">Entrar como invitado</a></span>
 		</div>
+		<!-- </div> -->
+	</div>
 </body>
 </html>
--- Anigram/App/views/registro.php
+++ Anigram/App/views/registro.php
@@ -19,37 +19,38 @@
             <h3><img src="../../public/img/ic_keyboard_arrow_left_black_24px.svg" alt="" /><a href="login.php">Volver</a></h3>
         </div>
         <div class="row logo"><img src="../../public/img/Logo-Nombre.png" alt="" /></div>
-
-        <form action="../controllers/gestionaRegistroUsuario.php" method="post" enctype="multipart/form-data">
-            <div class="row">
-                <div class="contenedor contedor-izquierdo col-md-12 col-lg-5">
-                    <?php include 'registroUsuario.php' ?>
-                </div>
-                <div class="contenedor contedor-derecho separador col-md-12 col-lg-6 offset-lg-1">
-                    <ul class="nav nav-tabs select-tabs">
-                        <li class="nav-item">
-                            <a id="Mascota" class="menu-tabs nav-link active" aria-label="div-mascota" href="#" onclick="selectTab('Mascota');">Mascota</a>
-                        </li>
-                        <li class="nav-item">
-                            <a id="Comercio" class="menu-tabs nav-link" href="#" aria-label="div-comercio" onclick="selectTab('Comercio');">Comercio</a>
-                        </li>
-                    </ul>
-
-                    <div id="div-Mascota" class="tab-content active">
-                        <?php include 'registroMascota.php' ?>
+        <div class="row registro">
+            <form action="../controllers/gestionaRegistroUsuario.php" method="post" enctype="multipart/form-data">
+                <div class="row">
+                    <div class="contenedor contedor-izquierdo col-md-12 col-lg-5">
+                        <?php include 'registroUsuario.php' ?>
                     </div>
-                    <div id="div-Comercio" class="tab-content">
-                        <?php include 'registroComercio.php' ;
-                        ?>
+                    <div class="contenedor contedor-derecho separador col-md-12 col-lg-6 offset-lg-1">
+                        <ul class="nav nav-tabs select-tabs">
+                            <li class="nav-item">
+                                <a id="Mascota" class="menu-tabs nav-link active" aria-label="div-mascota" onclick="selectTab('Mascota');">Mascota</a>
+                            </li>
+                            <li class="nav-item">
+                                <a id="Comercio" class="menu-tabs nav-link" aria-label="div-comercio" onclick="selectTab('Comercio');">Comercio</a>
+                            </li>
+                        </ul>
+
+                        <div id="div-Mascota" class="tab-content active">
+                            <?php include 'registroMascota.php' ?>
+                        </div>
+                        <div id="div-Comercio" class="tab-content">
+                            <?php include 'registroComercio.php' ;
+                            ?>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div id="boton_enviar" >
-                    <input id="submit" type="submit" name="submit"  onchange="muestraImagen" class="submitHueso" value="Guardar"/>	
+                <div class="row">
+                    <div id="boton_enviar" >
+                        <input id="submit" type="submit" name="submit"  onchange="muestraImagen" class="submitHueso" value="Guardar"/>	
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
 </body>
 </html>
